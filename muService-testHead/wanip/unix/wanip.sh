ed38746c5282a4dd922a85c4bf8f44760dacbd7b
tkohegyi <tamas_kohegyi@epam.com>
2022-10-09 14:06:14 +0200

    wanip test head unix added

---
+++ muService-testHead/wanip/unix/wanip.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#need libs installed:
+# - wget
+# - curl
+# and this script installed at /opt/mu-service/wanipcheck folder
+# and fill the settings below properly
+MYID=<INSTERT_TEST_HEAD_ID_HERE>
+MYSERVER=https://muService.magyar.website/secure/uploadData
+
+#clean up
+cd /opt/mu-service/wanipcheck
+rm -f index.html
+rm -f wget.tmp
+rm -f myip.txt
+
+#get my IP
+wget http://getmyipaddress.org -o wget.tmp --no-dns-cache --no-cache
+#this will create an index.html
+
+#now get my ip
+cat index.html| grep "Your IP Address"| tr ' ' '\n'| tr '<' '\n'| grep "\." | tr -d ' ' > myip.txt
+MYIP=`cat myip.txt| tr -d ' '`
+
+#if my ip is empty: CRITICAL
+if [ "$MYIP" == "" ]; then
+    echo "CRITICAL, Cannot detect My WAN IP|0.0.0.0"
+    mv -f myip.txt myip.txt.old
+    mv -f index.html index.html.old
+    exit 2
+fi
+
+#if my ip is new: WARNING
+OLDIP=`cat myip.txt.old`
+if [ "$MYIP" != "$OLDIP" ]; then
+    echo "WARNING, My WAN IP is changed from '$OLDIP' to '$MYIP'|$MYIP"
+    #preserve old ip for next use
+    mv -f myip.txt myip.txt.old
+    mv -f index.html index.html.old
+    #upload new info
+    MYSTATEMENT=`echo -n "{ \"id\": \"";echo -n $MYID;echo "\", \"head\":\"wanipcheck\", \"status\": \"OK\", \"information\": \"";echo -n $MYIP;echo -n "\" }"`
+    curl -X POST -H "Content-Type: application/json" -d $MYSTATEMENT $MYSERVER
+    exit 1
+fi
+
+echo "OK, My WAN IP is:$MYIP|$MYIP"
+mv -f index.html index.html.old
+
+exit 0

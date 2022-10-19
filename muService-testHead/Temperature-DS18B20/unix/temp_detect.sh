bee617baec9e0aa01494c1d30bfe645f2e1dde0f
tkohegyi <tamas_kohegyi@epam.com>
2022-10-19 22:16:33 +0200

    making temp sensor unix script workable

--- muService-testHead/Temperature-DS18B20/unix/temp_detect.sh
+++ muService-testHead/Temperature-DS18B20/unix/temp_detect.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+#this script checks the temperature
+#this script should work via crontab in every minute
 
+#need libs installed:
+# - wget
+# and this script installed at /opt/mu-service/ds18b20 folder
+# and fill the settings below properly
+MYID=
+MYSERVER=https://muService.magyar.website/secure/uploadData
 #ID of the temp sensor
 TEMP_DEV=
-#ID of the test head
-TH=
 
 cd /sys/bus/w1/devices/$TEMP_DEV
-
-ULIMIT_WARN=30000
-ULIMIT_CRIT=40000
-LLIMIT_WARN=0
-LLIMIT_CRIT=-2000
-
 
 #get the temperature
 ZA=`cat w1_slave`
@@ -21,26 +21,11 @@ Z=`echo $ZA| grep -E -o ".{0,0}t=.{0,5}"|cut -c 3-`
 
 SENSORTEMP=`echo "scale=3; x=$Z/1000; if(x==0) print \"0.0\" else if(x>0 && x<1) print 0,x else if(x>-1 && x<0) print \"-0\",-x else print x" | bc`
 
-#if lower or over certain limit, then CRITICAL
-if [ $ULIMIT_CRIT -lt $Z ]; then
-    echo $ZA > /tmp/$THCritical.txt
-    exit 2
-fi
-if [ $LLIMIT_CRIT -gt $Z ]; then
-    echo $ZA > /tmp/$THCritical.txt
-    exit 2
-fi
+cd /opt/mu-service/ds18b20
+echo $SENSORTEMP > "$MYID"_temp.txt
 
-
-#if lower or over certain limit, then WARNING
-if [ $ULIMIT_WARN -lt $Z ]; then
-    echo $ZA > /tmp/$THWarning.txt
-    exit 1
-fi
-if [ $LLIMIT_WARN -gt $Z ]; then
-    echo $ZA > /tmp/$THWarning.txt
-    exit 1
-fi
-
-echo $ZA > /tmp/$THNormal.txt
+#upload new data report
+MYSTATEMENT=`echo -n "{ \"id\": \"";echo -n $MYID;echo -n "\", \"head\":\"ds18b20\", \"status\": \"OK\", \"information\": \"";echo -n $SENSORTEMP;echo -n "\" }"`
+echo "MYSTATEMENT $MYSTATEMENT"
+curl -X POST -H "Content-Type: application/json" -d "$MYSTATEMENT" $MYSERVER
 exit 0
